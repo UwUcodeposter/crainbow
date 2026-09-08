249b911f8d4d9d9dc4d90b69f75747113f3e4ffd
Hallie <fpsgameplaye@gmail.com>
2026-09-08 16:12:29 +0700

    Create crainbow.rb

---
+++ Formula/crainbow.rb
@@ -0,0 +1,11 @@
+class Crainbow < Formula
+  desc "Print command-line text with random ANSI colors"
+  homepage "https://github.com/UwUcodeposter/crainbow"
+  url "https://github.com/UwUcodeposter/crainbow/releases/download/v1.0.0/crainbow-mac.tar.gz"
+  sha256 "PASTE_YOUR_SHA256_HASH_HERE"
+  license "MIT"
+
+  def install
+    bin.install "crainbow"
+  end
+end
