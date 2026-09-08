034c00aa8822c23e247d78f436488c1cac099c50
Hallie <fpsgameplaye@gmail.com>
2026-09-08 16:16:38 +0700

    Update Crainbow URL and SHA256 hash

--- Formula/crainbow.rb
+++ Formula/crainbow.rb
@@ -1,8 +1,8 @@
 class Crainbow < Formula
   desc "Print command-line text with random ANSI colors"
   homepage "https://github.com/UwUcodeposter/crainbow"
-  url "https://github.com/UwUcodeposter/crainbow/releases/download/v1.0.0/crainbow-mac.tar.gz"
-  sha256 "PASTE_YOUR_SHA256_HASH_HERE"
+  url "https://github.com/UwUcodeposter/crainbow/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "04faa65201cbcddacd4a521fa5db4058c62c8b88ddf5621ba6397339fcc7d6b1"
   license "MIT"
 
   def install
