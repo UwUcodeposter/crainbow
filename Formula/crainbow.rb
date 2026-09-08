74d4d3775370ae16859e21eebb636d510d710a9f
Hallie <fpsgameplaye@gmail.com>
2026-09-08 16:19:03 +0700

    SHA
    SHA WAS WRONG

--- Formula/crainbow.rb
+++ Formula/crainbow.rb
@@ -2,7 +2,7 @@ class Crainbow < Formula
   desc "Print command-line text with random ANSI colors"
   homepage "https://github.com/UwUcodeposter/crainbow"
   url "https://github.com/UwUcodeposter/crainbow/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "04faa65201cbcddacd4a521fa5db4058c62c8b88ddf5621ba6397339fcc7d6b1"
+  sha256 "0cc590e6194b04f711dc8d032b20e81f03b7ca436d7438d0aad91230440722e7"
   license "MIT"
 
   def install
